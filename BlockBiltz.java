33584bea2b521e8ef2de0be92b9d7cd5f0f11cf2
Jefferson Lee <jeff3456@gmail.com>
2014-04-12 01:43:09 -0400

    Update BlockBiltz.java

--- BlockBiltz.java
+++ BlockBiltz.java
@@ -1,8 +1,8 @@
 import processing.core.PApplet;
-public class Dot extends PApplet
+public class BlockBlitz extends PApplet
 {
 	int numBlocks = 1;
-	Blocks [] a = new Blocks[150];
+	Blocks [] a = new Blocks[300];
 	boolean gameOver = false;
 	long startTime =  System.currentTimeMillis();
 	int timeElasped;
@@ -12,9 +12,9 @@ public class Dot extends PApplet
 	public void setup()
 	{
 		size(500,1000);
-		for(int i = 0; i < 150; i++)
+		for(int i = 0; i < 300; i++)
 		{
-			a[i] = new Blocks();
+			a[i] = new Blocks(this);
 		}
 		
 	}
@@ -28,24 +28,30 @@ public class Dot extends PApplet
 			fill(255,255,255);
 			rect(mouseX,mouseY,10,10);
 		}
+		
 	}
 	public void drawBlocks()
 	{
 		timeElasped = (int) (System.currentTimeMillis() / 1000 - startTime / 1000);
-		if( (timeElasped != previousTime))
+		if(timeElasped != previousTime)
 		{
 			numBlocks++;
 			previousTime = timeElasped;
 		}
-
+		
+		
 		for(int i = 0; i < numBlocks; i++)
 		{
 			int xPos = (int)(a[i].xpos * 500);
+			if(xPos > (500 - a[i].size))
+			{
+				xPos = xPos - a[i].size;
+			}
 			int yPos = (int)(a[i].ypos);
-			fill(a[i].r, a[i].g , a[i].b);
+			fill(a[i].colora, a[i].colorb , a[i].colorc);
 			rect(xPos, yPos, a[i].size, a[i].size);
 			a[i].move();
-			a[i].resetBlock(1000);
+			a[i].resetBlock(1000, this);
 			if((mouseX <= xPos +  a[i].size && mouseX >= xPos - 10) && (mouseY <= yPos + a[i].size && mouseY >= yPos - 10))
 			{
 				gameOver = true;
@@ -56,7 +62,12 @@ public class Dot extends PApplet
 		{
 			timeElasped = (int) (System.currentTimeMillis() / 1000 - startTime / 1000);
 			println("you survived " + timeElasped + " seconds");
+			println("you died on level " + numBlocks);
 			println("you dodged " + Blocks.score + " blocks");
 		}
+	}
+	public void keyPressed()
+	{
+		numBlocks++;
 	}
 }
